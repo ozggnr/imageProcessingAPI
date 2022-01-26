7ad2c4f20a224d87f893cef16a22ac06f3af63ba
ozggnr <ozgecaylioglu@gmail.com>
2022-01-25 22:51:07 -0500

    added cache and checked if file is exist

--- build/routes/api/images.js
+++ build/routes/api/images.js
@@ -1,12 +1,22 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
@@ -14,8 +24,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const sharp_1 = __importDefault(require("sharp"));
+const fs = __importStar(require("fs"));
 const images = express_1.default.Router();
-images.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+images.get('/', (req, res) => {
     //query paramters
     let filename;
     let height;
@@ -25,14 +36,23 @@ images.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     width = Number(req.query.width);
     //We need a file name with valid width and height to resize and send back to the browser otherwise it will shows the error page in browser
     if (filename && width > 0 && height > 0) {
-        (0, sharp_1.default)(`public/originalImages/${filename}.jpeg`).resize(width, height)
-            .toFile(`public/resizedImages/${filename}-${width}x${height}.jpeg`, (err, info) => {
-            const currDir = process.cwd();
-            res.sendFile(`${currDir}/public/resizedImages/${filename}-${width}x${height}.jpeg`);
+        const currDir = process.cwd();
+        const outPath = `${currDir}/public/resizedImages/${filename}-${width}x${height}.jpeg`;
+        //To cache and load from there if file is already exist
+        fs.access(outPath, err => {
+            if (!err) {
+                res.sendFile(outPath);
+            }
+            else {
+                (0, sharp_1.default)(`public/originalImages/${filename}.jpeg`).resize(width, height)
+                    .toFile(`public/resizedImages/${filename}-${width}x${height}.jpeg`, (err, info) => {
+                    res.sendFile(outPath);
+                });
+            }
         });
     }
     else {
         res.send("<h2>Missing information! Please be sure if you provide all the informations</h2>");
     }
-}));
+});
 exports.default = images;
--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import sharp from 'sharp';
+import * as fs from 'fs';
 
 const images = express.Router();
 
-images.get('/', async (req, res) => {
+images.get('/', (req, res) => {
     //query paramters
     let filename: string
     let height: number
@@ -13,11 +14,19 @@ images.get('/', async (req, res) => {
     width = Number(req.query.width)
     //We need a file name with valid width and height to resize and send back to the browser otherwise it will shows the error page in browser
     if (filename && width > 0 && height > 0) {
-        sharp(`public/originalImages/${filename}.jpeg`).resize(width, height)
-        .toFile(`public/resizedImages/${filename}-${width}x${height}.jpeg`, (err, info) => {
-            const currDir = process.cwd()
-            res.sendFile(`${currDir}/public/resizedImages/${filename}-${width}x${height}.jpeg`)
-        })   
+        const currDir = process.cwd()
+        const outPath = `${currDir}/public/resizedImages/${filename}-${width}x${height}.jpeg`
+        //To cache and load from there if file is already exist
+        fs.access(outPath, err => {
+            if(!err) {
+                res.sendFile(outPath)
+            } else {
+                sharp(`public/originalImages/${filename}.jpeg`).resize(width, height)
+                .toFile(`public/resizedImages/${filename}-${width}x${height}.jpeg`, (err, info) => {
+                    res.sendFile(outPath)
+                })
+            }
+        }) 
     } else {
         res.send("<h2>Missing information! Please be sure if you provide all the informations</h2>")
     }
