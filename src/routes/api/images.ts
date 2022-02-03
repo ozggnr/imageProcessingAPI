0aff895fb6a03c85f12c53113c7c408e1f3542c2
ozggnr <ozgecaylioglu@gmail.com>
2022-02-02 22:34:43 -0500

    refactor with eslint

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -6,28 +6,26 @@ const images = express.Router();
 
 images.get('/', (req, res) => {
     //query paramters
-    let filename: string
-    let height: number
-    let width: number
-    filename = req.query.filename as string
-    height = Number(req.query.height)
-    width = Number(req.query.width)
+    const filename = req.query.filename as string;
+    const height = Number(req.query.height);
+    const width = Number(req.query.width);
+
     //We need a file name with valid width and height to resize and send back to the browser otherwise it will shows the error page in browser
     if (filename && width > 0 && height > 0) {
-        const currDir = process.cwd()
-        const outPath = `${currDir}/public/resizedImages/${filename}-${width}x${height}.jpeg`
+        const currDir = process.cwd();
+        const outPath = `${currDir}/public/resizedImages/${filename}-${width}x${height}.jpeg`;
         //To cache and load from there if file is already exist
         fs.access(outPath, async (err) => {
             if(!err) { res.sendFile(outPath) } 
             else {
                 await resize(filename, width, height).catch((err: Error) => { 
-                    res.send(`<h2>File missing</h2>`) 
+                    res.send(`<h2>${err}</h2>`);
                 })
-                res.sendFile(outPath)
+                res.sendFile(outPath);
             }
         }) 
     } else {
-        res.send("<h2>Missing or wrong information! Please be sure if you provide all the informations</h2>")
+        res.send("<h2>Missing or wrong information! Please be sure if you provide all the informations</h2>");
     }
 })
 
