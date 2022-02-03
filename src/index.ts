f15b3b49e7573ee1235da885e65bb1a9ce18fbdf
ozggnr <ozgecaylioglu@gmail.com>
2022-02-02 21:44:18 -0500

    carried port listen in server file

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import express from 'express';
 import routes from './routes/index';
 
 const app = express();
-const port = 5000;
 //main route
 app.use('/api', routes);
 
-app.listen(port, () => {
-    console.log(`Listening port ${port}`);
-})
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,4 @@
+import app from './index';
+//port is carried here to prevent test from listening the same port
+const port = 5000;
+app.listen(port, () => console.log(`Listening port ${port}`))
